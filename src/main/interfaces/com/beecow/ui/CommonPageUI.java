c48fb0a3460037d0e4aebae57eee37150e076f53
xuanhoangdinh <48581378+xuanhoangdinh@users.noreply.github.com>
2019-06-06 14:20:21 +0700

    1

--- src/main/interfaces/com/beecow/ui/CommonPageUI.java
+++ src/main/interfaces/com/beecow/ui/CommonPageUI.java
@@ -3,6 +3,6 @@ package com.beecow.ui;
 public class CommonPageUI {
 	public static final String DYNAMIC_PAGES = "//a[contains(text(),'%s')]";
 	public static final String DYNAMIC_MSG = "//*[contains(text(),'%s')]";
-	public static final String DYNAMIC_LBL = "//*[contains(text(),'%s')]";
+	public static final String DYNAMIC_LBL = "//div[@class='name']//*[contains(text(),'%s')]";
 
 }
